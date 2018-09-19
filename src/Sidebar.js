e06aefcc9ae67742dcef211d87a75a9135358c03
torsku <torsku@stud.ntnu.no>
2018-09-19 14:38:21 +0200

    created input forms for sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,27 +2,110 @@ import React, {Component} from 'react';
 import './Sidebar.css';
 
 class Sidebar extends Component{
+    constructor(props){
+        super(props);
+        this.state = {optionMusic: '', optionPoetry:'', optionArt: ''};
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event){
+        const target = event.target;
+        const name = target.name;
+        if(name === "music") this.setState({optionMusic: target.value});
+        else if(name === "poetry") this.setState({optionPoetry: target.value});
+        else if (name === "art") this.setState({optionArt: target.value});
+        console.log(this.state);
+    }
     render(){
         return(
             <div className="wrapper">
                 <div className="sidebar">
-                    <ul>
-                        <ul> Category1
-                            <li>Option1</li>
-                            <li>Option2</li>
-                            <li>Option3</li>
-                        </ul>
-                        <ul> Category2
-                            <li>Option1</li>
-                            <li>Option2</li>
-                            <li>Option3</li>
-                        </ul>
-                        <ul> Category3
-                            <li>Option1</li>
-                            <li>Option2</li>
-                            <li>Option3</li>
-                        </ul>
-                    </ul>
+                    <form>
+                        <label> Select preferred music:
+                        <br />
+                            <input
+                                name="music"
+                                value="heavy metal"
+                                type="radio"
+                                onChange={this.handleChange}
+                            />
+                            heavy metal
+                        <br />
+                            <input
+                                name="music"
+                                value="smooth jazz"
+                                type="radio"
+                                onChange={this.handleChange}
+                            />
+                            smooth jazz
+                        <br />
+                            <input
+                                name="music"
+                                value="classical"
+                                type="radio"
+                                onChange={this.handleChange}
+                            />
+                            classical
+                        </label>
+                    </form>
+                    <br />
+                    <form>
+                        <label> Select preferred poetry:
+                            <br />
+                            <input
+                                name="poetry"
+                                value="romantic"
+                                type="radio"
+                                onChange={this.handleChange} 
+                            />
+                            romantic
+                            <br />
+                            <input
+                                name="poetry"
+                                value="comedy"
+                                type="radio"
+                                onChange={this.handleChange} 
+                            />
+                            comedy
+                            <br />
+                            <input
+                                name="poetry"
+                                value="revolutionary"
+                                type="radio"
+                                onChange={this.handleChange} 
+                            />
+                            revolutionary
+                        </label>
+                    </form>
+                    <br />
+                    <form>
+                        <label> Select preferred art:
+                            <br />
+                            <input
+                                name="art"
+                                value="the wonder that is the car"
+                                type="radio"
+                                onChange={this.handleChange} 
+                            />
+                            The wonder that is the car
+                            <br />
+                            <input
+                                name="art"
+                                value="something something we don't understand but seems important"
+                                type="radio"
+                                onChange={this.handleChange} 
+                            />
+                            something we don't understand but seems important
+                            <br />
+                            <input
+                                name="art"
+                                value="this we really dont't understand"
+                                type="radio"
+                                onChange={this.handleChange} 
+                            />
+                            this we really don't understand
+                        </label>
+                    </form>
                 </div>
             </div>
         );
