4f8dc0db3d96ef2290265ea368b2b8d723bb11f2
tjabejohannes <tjabejv@stud.ntnu.no>
2018-09-19 09:35:47 +0200

    Adding some scaleability with css grid and layout configuring #2

--- src/Content.js
+++ src/Content.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 import './Content.css';
+import View from './View.js';
+import Sidebar from './Sidebar.js';
+import PoeAThree from './PoeATree.js';
+
 class Content extends Component{
     render(){
         return(
-            <div className="wrapper">
+            <div className="contentWrapper">
+                <div className="viewContent">
+                    <View view={this.props.View}/>
+                </div>
+                <div className="sidebarContent">
+                    <Sidebar Sidebar={this.props.Sidebar}/>
+                </div>
+                <div className="poeATreeContent">
+                    <PoeAThree PoeAThree={this.props.PoeAThree}/>
+                </div>
             </div>
         );
     }
--- src/Main.js
+++ src/Main.js
@@ -1,37 +1,30 @@
 import React, { Component } from 'react';
 import './Main.css';
 import Content from './Content.js';
-import View from './View.js';
-import Sidebar from './Sidebar.js';
-import Music from "./music";
 
 class Main extends Component{
     render(){
         return(
-            <div className="wrapper">
+            <div className="mainWrapper">
                 <div className="nav">
                     <ul>
                         <li>
-                            <a href="#">Display1</a>
+                            <a href="#">Savepoint 1</a>
                         </li>
                         <li>
-                            <a href="#">Display2</a>
+                            <a href="#">Savepoint 2</a>
                         </li>
                         <li>
-                            <a href="#">Display3</a>
+                            <a href="#">Savepoint 3</a>
                         </li>
                         <li>
-                            <a href="#">Display4</a>
-                        </li>
-                        <li>
-                            <a href="#">About</a>
+                            <a href="#">Savepoint 4</a>
                         </li>
                     </ul>
                 </div>
-                <Content/>
-                <View />
-                <Music/>
-                <Sidebar />
+                <div className="contentMain">
+                    <Content content={this.props.Content}/>
+                </div>
             </div>
         );
     }
