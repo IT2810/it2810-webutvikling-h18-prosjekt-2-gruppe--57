cc03f89fa1c2cefbd0f6b3e7f3a213678478cb87
tjabejohannes <tjabejv@stud.ntnu.no>
2018-09-19 13:06:03 +0200

    Romoving components

---
+++ src/trash/PoeATree.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import './PoeATree.css';
+
+class View extends Component {
+    render(){
+        return(
+
+        )
+    }
+}
+export default View;
---
+++ src/trash/View.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './View.css';
+
+class View extends Component {
+    render() {
+        return (
+
+        )
+    }
+}
+
+export default View;
